402c25523ca28d39e19594b209813ea94268dd83
Pragnay2704 <pranb.2004@gmail.com>
2026-04-11 11:53:12 +0530

    Updated project files

--- backend/user-service/src/test/java/HashGen.java
+++ backend/user-service/src/test/java/HashGen.java
@@ -3,6 +3,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class HashGen {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println("HASH: " + encoder.encode("Student@123"));
+        System.out.println("ADMIN HASH: " + encoder.encode("Admin@123"));
     }
 }
